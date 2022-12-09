01e83689a88e731fdfeaf52391e2c8de7d8f4d07
Mimshad1999 <119530266+Mimshad1999@users.noreply.github.com>
2022-12-09 17:24:53 +0530

    Add files via upload

---
+++ Lab_Test/oddNumber.java
@@ -0,0 +1,18 @@
+package labproject;
+
+public class oddNumber {
+	public static void main(String args[])   
+	{  
+	int number=20;  
+	System.out.print("List of odd numbers from 1 to "+number+": ");  
+	for (int i=1; i<=number; i++)   
+	{  
+	
+	if (i%2!=0)   
+	{  
+	System.out.print(i + " ");  
+	}  
+	}  
+	} 
+
+}
